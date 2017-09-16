63131cd61f7134e463e857398b013a8e827140f8
yzc <18107153152@163.com>
2017-09-16 11:49:26 +0800

    helf mylife

--- src/maincompts/myLife.vue
+++ src/maincompts/myLife.vue
@@ -8,38 +8,38 @@
       <div class="warpperHover">
         <li class="dropBitItem" v-for="(item,index) in dropBitItem">
           <img :src="item.imgUrl" @mouseenter.stop="warpperEnter" @mouseleave.stop="warpperLeave" class="dB" :class="{ active: isActive }" alt="瞄^^">
-          <span v-if="display">hello world!</span>
+          <span v-if="display">{{ item.info }}</span>
         </li>
       </div>
     </div>
 		<div class="bigStory">
       <div class="bigStoryHeader">
-        <h2>{{turnPoint}}</h2>
-        <span></span>
+        <h2> {{turnPoint}} </h2>
+        <span> {{bitLowInf}} </span>
       </div>
       <div class="warpperHover">
         <li class="bigStoryItem" v-for="(item,index) in bigStoryItem">
           <img :src="item.imgUrl" class="bSI">
-          <span>hello world!</span>
+          <span>{{ item.info }}</span>
         </li>
       </div>
     </div>
     <div class="interest">
       <div class="interestHeader">
           <h2>{{interest}}</h2>
-          <span></span>
+          <span>{{bitLowInf}}</span>
       </div>
       <div class="warpperHover">
         <li class="interestItem" v-for="(item,index) in bigStoryItem">
           <img :src="item.imgUrl" class="iI">
-          <span>hello world!</span>
+          <span>{{ item.info }}</span>
         </li>
       </div>
     </div>
     <div class="messy">
       <div class="messyHeader">
         <h2>{{chaos}}</h2>
-        <span></span>
+        <span>{{bitLowInf}}</span>
       </div>
       <img class="messyItem" v-for="(item,index) in messyItem" >     
     </div>
@@ -60,70 +60,90 @@ export default {
       display: true,
       dropBitItem: [
         {
-          imgUrl: require('../assets/images/dropbit/db0.jpg')
+          imgUrl: require('../assets/images/dropbit/db0.jpg'),
+          info: '111'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db1.jpg')
+          imgUrl: require('../assets/images/dropbit/db1.jpg'),
+          info: '222'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db2.jpg')
+          imgUrl: require('../assets/images/dropbit/db2.jpg'),
+          info: '333'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db3.jpg')
+          imgUrl: require('../assets/images/dropbit/db3.jpg'),
+          info: '444'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db4.jpg')
+          imgUrl: require('../assets/images/dropbit/db4.jpg'),
+          info: '555'
         }
       ],
       bigStoryItem: [
         {
-          imgUrl: require('../assets/images/dropbit/db0.jpg')
+          imgUrl: require('../assets/images/dropbit/db0.jpg'),
+          info: '666'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db1.jpg')
+          imgUrl: require('../assets/images/dropbit/db1.jpg'),
+          info: '777'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db2.jpg')
+          imgUrl: require('../assets/images/dropbit/db2.jpg'),
+          info: '888'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db3.jpg')
+          imgUrl: require('../assets/images/dropbit/db3.jpg'),
+          info: '999'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db4.jpg')
+          imgUrl: require('../assets/images/dropbit/db4.jpg'),
+          info: '101010'
         }
       ],
       interestItem: [
         {
-          imgUrl: require('../assets/images/dropbit/db0.jpg')
+          imgUrl: require('../assets/images/dropbit/db0.jpg'),
+          info: '111'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db1.jpg')
+          imgUrl: require('../assets/images/dropbit/db1.jpg'),
+          info: '222'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db2.jpg')
+          imgUrl: require('../assets/images/dropbit/db2.jpg'),
+          info: '333'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db3.jpg')
+          imgUrl: require('../assets/images/dropbit/db3.jpg'),
+          info: '444'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db4.jpg')
+          imgUrl: require('../assets/images/dropbit/db4.jpg'),
+          info: '555'
         }
       ],
       messyItem: [
         {
-          imgUrl: require('../assets/images/dropbit/db0.jpg')
+          imgUrl: require('../assets/images/dropbit/db0.jpg'),
+          info: '111'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db1.jpg')
+          imgUrl: require('../assets/images/dropbit/db1.jpg'),
+          info: '222'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db2.jpg')
+          imgUrl: require('../assets/images/dropbit/db2.jpg'),
+          info: '333'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db3.jpg')
+          imgUrl: require('../assets/images/dropbit/db3.jpg'),
+          info: '444'
         },
         {
-          imgUrl: require('../assets/images/dropbit/db4.jpg')
+          imgUrl: require('../assets/images/dropbit/db4.jpg'),
+          info: '555'
         }
       ]
     }
